e26462dc87a35adf6bd677e40c39120ca3dbbfc7
SIHFANCHENTUF\sihfa <sihfanchen@gmail.com>
2025-12-05 15:27:41 +0800

    修改 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 請將 'your-repo-name' 替換為您在 GitHub 上建立的儲存庫名稱。
+// 如果您的網址是 username.github.io (沒有子路徑)，則設為 '/'。
+const BASE_PATH = '/cpm/'; // 儲存庫名稱已設定為 cpm
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: BASE_PATH, // 設定專案的基底公開路徑
+  build: {
+    // 確保輸出的檔案路徑正確
+    outDir: 'dist',
+  }
 })
