2e2ec0c2480e15b0c731afe2ef40d248f4de23a3
SIHFANCHENTUF\sihfa <sihfanchen@gmail.com>
2025-12-05 15:55:24 +0800

    增加 dist 資料夾

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 請將 'your-repo-name' 替換為您在 GitHub 上建立的儲存庫名稱。
-// 如果您的網址是 username.github.io (沒有子路徑)，則設為 '/'。
-const BASE_PATH = '/cpm/'; // 儲存庫名稱已設定為 cpm
+// 必須是 /cpm/，因為您的儲存庫名稱是 cpm
+const BASE_PATH = '/cpm/'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: BASE_PATH, // 設定專案的基底公開路徑
+  base: BASE_PATH, // <--- 這裡必須是 /cpm/
   build: {
-    // 確保輸出的檔案路徑正確
     outDir: 'dist',
   }
 })
